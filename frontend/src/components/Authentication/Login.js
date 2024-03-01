de041166e8cdf69a92dbb628c8fd1ed00d7361e6
shreyas710 <shreyasnk5321@gmail.com>
2024-02-29 20:56:40 -0800

    api endpoint changed

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "http://localhost:8000/api/user/login",
+        "/api/user/login",
         { email, password },
         config
       );
--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -51,7 +51,7 @@ const Signup = () => {
         },
       };
       const { data } = await axios.post(
-        "http://localhost:8000/api/user",
+        "/api/user",
         {
           name,
           email,
